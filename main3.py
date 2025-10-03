5aa4a4b184dfbee127b95eb82d0abe0cdfec2047
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-10-03 15:35:22 +0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -2,4 +2,5 @@ familya = input("familyaningzni kiriting: ").lower()
 ism = input("ismingizni kiriting: ").lower()
 sharif = input("sharifingizni kiriting: ").lower()
 
-print(f" {ism} \n {familya} \n {sharif}")
+
+print(f" {ism} {familya} {sharif}")
