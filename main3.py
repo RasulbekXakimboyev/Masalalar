0fd2aa9d23a7a852061abbe01d4406b83cd2f8e7
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-10-03 13:34:17 +0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,5 @@
+familya = input("familyaningzni kiriting: ").lower()
+ism = input("ismingizni kiriting: ").lower()
+sharif = input("sharifingizni kiriting: ").lower()
+
+print(f" {ism} \n {familya} \n {sharif}")
