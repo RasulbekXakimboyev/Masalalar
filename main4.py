9abf4a06ca8079b86d03939c3d9199993aaa80c3
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-10-03 14:31:25 +0300

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,59 @@
+## 1
+# a = int(input("birinchi sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# sum = a + b
+# print(f" javobi: {sum}")
+
+## 2
+# a = int(input("uzunlikni kiriting: "))
+# b = int(input("enini kiriting: "))
+# c = a * b
+# print(f"yuzi: {c}")
+
+## 3
+# a = int(input("birinchi sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# c = int(input("uchinchi sonni kiriting: "))
+# d = (a * b * c) / 3
+# print(f"o'rta alifmetik: {d} ")
+
+## 4
+# a = int(input("birinchi tamoni kiriting: "))
+# b = int(input("ikkinchi tamoni kiriting "))
+# c = 2 * (a + b)
+# print(f"perimetri: {c}")
+
+## 5
+# a = int(input("necha soat: "))
+# b = int(input("necha daqiqa: "))
+# c = a * 60 + b
+# print(f"{c} daqiqa")
+
+## 6
+# a = int(input("qancha dolir pullingiz borligini kiriting: "))
+# b = a * 12000
+# print(f"{b} so'm")
+
+## 7
+# a = int(input("birinchi sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# c = abs(a - b)
+# print(f"{c}")
+
+## 8
+# a = int(input("qaysi sonni kirit moqchi siz: "))
+# b = int(input("qaysi darajaga ko'tarmoqchi siz: "))
+# c = a ** b
+# print(f"{a} nig {b} darajasi: {c}")
+
+## 9
+# a = int(input("birinchi sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# c = a // b
+# print(f"qoldiq: {c}")
+
+## 10
+# a = int(input("almashtirmoqchi bolgan sonni kiriting: "))
+# b = int(input("almashtirish bo'lgan sonni kiriting: "))
+# a, b = b, a
+# print(f"natija: {a,b}")
